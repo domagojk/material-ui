4da139a32a15f17f1f63f560f950e5775fd56bd6
Domagoj <domagoj.kriskovic@gmail.com>
2015-08-01 16:17:54 +0200

    update

--- src/mixins/classable.js
+++ src/mixins/classable.js
@@ -1,5 +1,5 @@
 let React = require('react');
-let classNames = require('classnames');
+//let classNames = require('classnames');
 
 
 module.exports = {
@@ -14,25 +14,25 @@ module.exports = {
     };
   },
 
-  getClasses(initialClasses, additionalClassObj) {
+  getClasses(initialClasses/*, additionalClassObj*/) {
     let classString = '';
 
     //Initialize the classString with the classNames that were passed in
     if (this.props.className.length) classString += ' ' + this.props.className;
 
     //Add in initial classes
-    if (typeof initialClasses === 'object') {
-      classString += ' ' + classNames(initialClasses);
-    }
-    else {
+    //if (typeof initialClasses === 'object') {
+    //  classString += ' ' + classNames(initialClasses);
+   // }
+    //else {
       classString += ' ' + initialClasses;
-    }
+    //}
 
     //Add in additional classes
-    if (additionalClassObj) classString += ' ' + classNames(additionalClassObj);
+    //if (additionalClassObj) classString += ' ' + classNames(additionalClassObj);
 
     //Convert the class string into an object and run it through the class set
-    return classNames(this.getClassSet(classString));
+    //return classNames(this.getClassSet(classString));
   },
 
   getClassSet(classString) {
